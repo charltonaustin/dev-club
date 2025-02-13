e17d05786c024aa4e1fed96d7a4d4911d8d8da44
charltonaustin <charlton.austin@kin.com>
2025-02-13 13:31:31 -0800

    Our solution

--- 2025-02-13/Ruby/lib/policy_number.rb
+++ 2025-02-13/Ruby/lib/policy_number.rb
@@ -1,3 +1,5 @@
+require_relative './constants'
+
 class PolicyNumber
 
   # @param rows [Array<String>]
@@ -18,5 +20,24 @@ class PolicyNumber
   end
 
   def close_digits
+    return_array = []
+    digits.each do |digit|
+      potential_matches = []
+      Constants::DIGITS.each do |k, v|
+        number = v.join
+        number_same = check_equal(number.chars, digit.join.chars)
+        potential_matches << k if number_same
+      end
+      return_array << potential_matches
+    end
+    return_array
+  end
+
+  def check_equal(chars, digit_chars)
+    number_same = 0
+    chars.each_with_index do |char, idx|
+      number_same += 1 if digit_chars[idx] == char
+    end
+    number_same >= 8
   end
 end
