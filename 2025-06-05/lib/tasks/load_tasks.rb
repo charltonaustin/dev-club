bfbfb2a2e27ff904715de5fd06ef830c791d073e
Tushar Samant <tushar.samant@kin.com>
2025-06-05 15:30:22 -0500

    Step 5: Cut reads over to the new interface

--- 2025-06-05/lib/tasks/load_tasks.rb
+++ 2025-06-05/lib/tasks/load_tasks.rb
@@ -3,7 +3,6 @@
 
 require "sqlite3"
 
-
 class LoadTasks
   extend T::Sig
   include Action
@@ -12,35 +11,31 @@ class LoadTasks
   def initialize(tasks, key)
     @tasks = tasks
     @key = key
-    @description = T.let('Load tasks', String)
+    @description = T.let("Load tasks", String)
     @db = T.let(SQLite3::Database.new("./db/tasks.db"), SQLite3::Database)
   end
 
   sig { override.returns(T::Array[TaskInterface]) }
   def do
-    puts 'Loaded tasks'
-    @db.execute "select * from task;" do |row|
+    puts "Loaded tasks"
+    @db.execute "select id, task_text, completed from task;" do |row|
       history = []
-      @db.execute "select * from history where task_id = ?", row[0] do |h_row|
-        history << h_row[0]
+      @db.execute "select task_id, task_text from history where task_id = ?", row[0] do |h_row|
+        history << h_row[1]
       end
       completed = false
-      if row[1] == 1
+      if row[2] == 1
         completed = true
       end
       @tasks << Task.new(row[1], completed, history)
     end
-    
+
     PressToContinue.new.do
     @tasks
   end
 
   sig { override.returns(String) }
-  def description
-    @description
-  end
+  attr_reader :description
   sig { override.returns(Integer) }
-  def key
-    @key
-  end
+  attr_reader :key
 end
