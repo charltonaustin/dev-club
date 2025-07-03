153ee4abc658475b0e5ebbca314bec5a9c055b5a
charltonaustin <charlton.austin@kin.com>
2025-07-03 13:05:32 -0700

    [Charlie|Sam|Tushar|Caleb] Updated to follow the sw dev club prompt.

--- 2025-07-03/producer/app/important_api.rb
+++ 2025-07-03/producer/app/important_api.rb
@@ -9,20 +9,16 @@ users = []
 
 post '/v1/user' do
   user = JSON.parse(request.body.read).transform_keys(&:to_sym)
+  if user[:first_name] && user[:given_name].nil?
+    user[:given_name] = user[:first_name]
+  end
   users.append(user)
   json success?: true, id: users.length
 end
 
 get '/v1/user/:id' do |id|
   id = id.to_i
-  json family_name: users[id - 1][:family_name], given_name: users[id - 1][:given_name]
-end
-
-patch '/v1/user/:id' do |id|
-  id = id.to_i
-  user = JSON.parse(request.body.read).transform_keys(&:to_sym)
-  users[id - 1].merge(user)
-  json family_name: users[id - 1][:family_name], given_name: users[id - 1][:given_name], success?: true
+  json family_name: users[id - 1][:family_name], first_name: users[id - 1][:given_name], given_name: users[id - 1][:given_name]
 end
 
 delete '/v1/user/:id' do |id|
